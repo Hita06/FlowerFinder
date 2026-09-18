1c1b71e1bf69eb4ade56e344f7b0fa62751b093b
Iris Chau <irischau83@gmail.com>
2026-09-18 13:17:52 +1200

    Update main navigation

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -1,52 +1,47 @@
 import 'package:flutter/material.dart';
+
 import '../theme.dart';
 
 class FlowerBottomNav extends StatelessWidget {
-  final int currentIndex;
-  final Function(int) onTap;
-
   const FlowerBottomNav({
     super.key,
     required this.currentIndex,
     required this.onTap,
   });
 
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: currentIndex,
       onTap: onTap,
-
       type: BottomNavigationBarType.fixed,
-
-      backgroundColor: AppColors.green,
-
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.white,
-
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-
-      elevation: 0,
-
+      backgroundColor: AppColors.cream,
+      selectedItemColor: AppColors.green,
+      unselectedItemColor: Colors.grey,
+      selectedFontSize: 12,
+      unselectedFontSize: 12,
       items: const [
         BottomNavigationBarItem(
-          icon: Icon(Icons.center_focus_strong),
-          label: 'Scan',
+          icon: Icon(Icons.camera_alt_outlined),
+          activeIcon: Icon(Icons.camera_alt),
+          label: 'Scanner',
         ),
-
         BottomNavigationBarItem(
-          icon: Icon(Icons.location_on),
-          label: 'Map',
+          icon: Icon(Icons.search_outlined),
+          activeIcon: Icon(Icons.search),
+          label: 'Search',
         ),
-
         BottomNavigationBarItem(
-          icon: Icon(Icons.menu_book),
+          icon: Icon(Icons.menu_book_outlined),
+          activeIcon: Icon(Icons.menu_book),
           label: 'Diary',
         ),
-
         BottomNavigationBarItem(
-          icon: Icon(Icons.person),
+          icon: Icon(Icons.person_outline),
+          activeIcon: Icon(Icons.person),
           label: 'Profile',
         ),
       ],
